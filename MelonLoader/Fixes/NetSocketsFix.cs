e4c88b684f193bb96019e288613107cea9c82bee
Trevor <25110388+TrevTV@users.noreply.github.com>
2022-12-11 11:03:50 -0500

    Fix NetSocketsFix
    I was stupid lol

--- MelonLoader/Fixes/NetSocketsFix.cs
+++ MelonLoader/Fixes/NetSocketsFix.cs
@@ -8,7 +8,7 @@ namespace MelonLoader.Fixes
 {
     internal static class NetSocketsFix
     {
-        internal static void Install() => Core.HarmonyInstance.Patch(typeof(SocketAsyncEventArgs).GetProperty("OnCompleted").GetGetMethod(), typeof(NetSocketsFix).GetMethod(nameof(DontRunMe)).ToNewHarmonyMethod());
+        internal static void Install() => Core.HarmonyInstance.Patch(typeof(SocketAsyncEventArgs).GetMethod("OnCompleted"), typeof(NetSocketsFix).GetMethod(nameof(DontRunMe)).ToNewHarmonyMethod());
         public static bool DontRunMe() => false;
     }
 }
